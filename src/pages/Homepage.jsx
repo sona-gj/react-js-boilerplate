77c348fbe509956d0bd65493fa9efe46e50c96ec
sona-gj <sonagj2020@gmail.com>
2025-05-12 11:58:20 -0400

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='Homepage'>
+        <h1>Welcome to Medlaunch Trial Portal</h1>
+    </div>
+  )
+}
+
+export default Homepage
